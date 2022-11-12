52d6e80bdf392612ce52ed980a677a715cc83a21
Nihalnihj <118068867+Nihalnihj@users.noreply.github.com>
2022-11-12 22:06:11 +0530

    Create Qwerty.m

---
+++ Qwerty.m
@@ -0,0 +1,9 @@
+- 👋 Hi, I’m @n.ihaaal
+- 👀 poy orghada
+- 🌱 I’m currently 8n dance vibe
+- 💞️ I’m looking to my love
+- 📫 How to kill me
+<!---
+Nihalnihj/Nihalnihj is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
